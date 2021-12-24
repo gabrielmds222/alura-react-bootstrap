b732380f96184645f489a32ff689420333e5a094
Gabriel <gabrielmedsilva@outlook.com>
2021-12-24 11:40:16 -0300

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // import Cadastro from './components/Formulario/Cadastro';
-import Teste from './components/Teste'
+import Formulario from './components/Formulario'
 import './App.css';
 
 function App() {
@@ -10,11 +10,23 @@ function App() {
         <h2> Formulário de Cadastro </h2>
         {/* <Cadastro/> */}
         <div className="row">
-          <Teste/>
+          <Formulario validarCPF={validarCPF}/>
         </div>
       </div>
     </>
   );
+}
+
+// function aoEnviarForm(dados){
+//   console.log(dados);
+// }
+
+function validarCPF(cpf){
+  if(cpf.length !== 11){
+    return {valido:false, texto:"CPF deve ter 11 digitos."}
+  }else{
+    return {valido:true, texto:""}
+  }
 }
 
 export default App;
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -3,20 +3,26 @@ import React, {useState} from 'react';
 
 import '../App.css';
 
-const Teste = () => {
+const Formulario = ({ validarCPF }) => {
     const [nome, setNome] = useState("");
     const [sobrenome, setSobrenome] = useState("");
+    const [cpf, setCpf] = useState("");
+    const [promocoes, setPromocoes] = useState(true);
+    const [novidades, setNovidades] = useState(true);
+    const [erros, setErros] = useState({cpf:{valido:true, texto:""}})
+    
     return ( 
         <form onSubmit={(event) => { 
             event.preventDefault()
-            console.log(nome, sobrenome)}}>
+            console.log(nome, sobrenome, cpf, promocoes, novidades)}}>
 
-<div class="mb-3">
+            <div class="mb-3">
                 <label for="formGroupExampleInput" class="form-label"> Nome </label>
                 <input 
                 type="text" 
                 class="form-control" 
                 id="formGroupExampleInput"
+
                 value={nome}
                 onChange={(event) => {
                     setNome(event.target.value);
@@ -30,6 +36,7 @@ const Teste = () => {
                 type="text" 
                 class="form-control" 
                 id="formGroupExampleInput2"
+
                 value={sobrenome}
                 onChange={(event) => {
                     setSobrenome(event.target.value);
@@ -43,17 +50,35 @@ const Teste = () => {
                 type="text" 
                 class="form-control" 
                 id="formGroupExampleInput2" 
+                error={!erros.cpf.valido}
+                helperText={erros.cpf.texto}
+
+                value={cpf}
+                onChange={(event) => {
+                    setCpf(event.target.value);
+                }} 
+
+                onBlur={(event)=>{
+                    const ehValido = validarCPF(cpf);
+                    setErros({cpf:ehValido})
+                  }}
                 />
             </div>
 
             <div className="row-switches">
                 <div class="form-check form-switch">
-                    <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+                    <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" checked={promocoes}
+                    onChange={(event) => {
+                        setPromocoes(event.target.checked)
+                    }}/>
                     <label class="form-check-label" for="flexSwitchCheckDefault">Promoções</label>
                 </div>
 
                 <div class="form-check form-switch">
-                    <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+                    <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" checked={novidades}
+                    onChange={(event) => {
+                        setNovidades(event.target.checked)
+                    }}/>
                     <label class="form-check-label" for="flexSwitchCheckDefault">Novidades</label>
                 </div>
                 </div>
@@ -62,4 +87,4 @@ const Teste = () => {
     );
 };
  
-export default Teste;
+export default Formulario;
